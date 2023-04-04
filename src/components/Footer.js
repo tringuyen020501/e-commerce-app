3f928e23c7be23b553d00c512f3786d71898925b
tringuyen252001@gmail.com <tringuyen252001@gmail.com>
2023-04-04 16:31:38 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+   return (
+      <>
+         <Footer></Footer>
+         <Footer></Footer>
+         <Footer></Footer>
+         <Footer></Footer>
+      </>
+   );
+};
+
+export default Footer;
