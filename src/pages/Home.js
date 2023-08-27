8a6c980fc7e54182a9bfbdec7879ff9d8aae9217
Nguyen Tran Minh Tri <tmtn@Nguyens-Laptop.local>
2023-08-27 16:17:39 +0700

    reload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -224,7 +224,7 @@ const Home = () => {
                         />
                         <div className="famous-content position-absolute">
                            <h5>Big Screen</h5>
-                           <h6>Smart Watch Series 7</h6>
+                           <h6>Smart Watch Series 5</h6>
                            <p>From $399or $16.62/mo. for 24 mo.*</p>
                         </div>
                      </div>
